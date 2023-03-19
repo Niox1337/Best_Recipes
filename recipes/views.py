2e5d45d6b44c2a043c9dad75ac640bb1595dcd2e
ZebedeeNye <2675732N@student.gla.ac.uk>
2023-03-19 17:39:10 +0000

    started making views page

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from recipes.models import Category, Page
+from recipes.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 # Create your views here.
+
+
