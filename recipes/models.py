8d262e049f4602d76af0d5e2d40c966287afb747
daly267 <123103187+daly267@users.noreply.github.com>
2023-03-20 22:48:11 +0000

    Update models

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,6 @@
+import django
 from django.db import models
-
+from datetime import date
 
 
 class UserProfile(models.Model):
@@ -16,14 +17,14 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=FIRST_NAME_LEN)
     last_name = models.CharField(max_length=LAST_NAME_LEN)
     email = models.EmailField(max_length=EMAIL_LEN)
-    date_of_birth = models.DateField()
+    date_of_birth = models.DateField(default=django.utils.timezone.now)
     # TODO: look at how rango handled user passwords etc.
     password = models.CharField(max_length=PASSWORD_LEN)
     user_description = models.TextField(max_length=USER_DESCRIPTION_LEN)
     profile_picture = models.ImageField()
 
     def __str__(self):
-        return self.name
+        return self.user_name
     
     class Meta:
         verbose_name_plural = "User Profiles"
@@ -37,8 +38,8 @@ class Recipe(models.Model):
     name = models.CharField(max_length=NAME_LEN, unique=True)
     text = models.TextField(max_length=TEXT_LEN)
     ingredients = models.TextField(max_length=INGREDIENTS_LEN)
-    views = models.IntegerField()
-    no_of_Ratings = models.IntegerField()
+    views = models.IntegerField(default=0)
+    no_of_ratings = models.IntegerField(default=0)
     recipe_picture = models.ImageField()
     saved_by = models.ManyToManyField(UserProfile)
 
@@ -50,26 +51,28 @@ class Review(models.Model):
     TEXT_LEN = 2000
 
     creator = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
     text = models.TextField(TEXT_LEN)
     image = models.ImageField()
-    no_of_ratings = models.IntegerField()
+    no_of_ratings = models.IntegerField(default=0)
     # TODO: minvaluevalidtor and maxvaluevalidtor?
-    review_rating = models.IntegerField(max)
+    review_rating = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.creator.user_name + " review " + str(self.id)
 
 class Rating(models.Model):
 
     creator = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
-    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, null=True, blank=True)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, null=True, blank=True)
     # True = recipe, False = review
-    recipe_or_review = models.BooleanField()
-    rating = models.IntegerField()
+    recipe_or_review = models.BooleanField(default=True)
+    # 5 star scale on 1-10 scale e.g 1 = 0.5 stars
+    rating = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.name
+        return self.creator.user_name + " rating " + str(self.id)
 
 class Tag(models.Model):
 
@@ -80,4 +83,4 @@ class Tag(models.Model):
     recipe = models.ManyToManyField(Recipe)
 
     def __str__(self):
-        return self.name
+        return self.tag
