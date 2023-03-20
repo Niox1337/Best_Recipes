e6ead2b4015114b1c3213f188f85ea7346febf71
Zhixiang <2616718f@student.gla.ac.uk>
2023-03-20 02:23:41 +0000

    Created home page urls, template

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    trending = {'trending': 'Trending'}
+    return render(request, 'recipes/index.html', context=trending)
