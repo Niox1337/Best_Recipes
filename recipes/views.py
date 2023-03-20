a41891c9dc05a5ef278c587237f704a521978edd
daly267 <123103187+daly267@users.noreply.github.com>
2023-03-20 23:17:57 +0000

    Fix URLs and views
    Current URLs and views caused runserver to fail

--- recipes/views.py
+++ recipes/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from recipes.models import Category, Page
-from recipes.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from recipes.models import UserProfile, Tag, Review, Rating, Recipe
 
 # Create your views here.
 
-
+def index(request):
+    response = render(request, 'recipes/index.html')
+    return response
